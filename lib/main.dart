6a087def5a50944ef622d0b688a907e46ca1ca68
JulianMozi <Jkoripamo@gmail.com>
2022-05-24 14:36:28 +0100

    updated background color

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         appBar: AppBar(
           backgroundColor: Colors.black,
           title: const Center(child: Text('Ask Me Anything')),
